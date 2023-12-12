138cc383bc98c7d1d26236921a5f2118a24428a5
Mirai500 <asifatumahbub001@gmail.com>
2023-12-12 12:56:28 +0100

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 const list = require('./100-data.js').list;
 
-const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(newList);
+const new_arr = list.map((cur, i) => cur * i);
+console.log(new_arr);
