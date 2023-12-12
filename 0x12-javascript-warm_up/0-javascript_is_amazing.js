998cfc760fd7d24aea13ea02b1a10d610783b345
Mirai500 <asifatumahbub001@gmail.com>
2023-12-12 13:01:52 +0100

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 
-/**
-
-* This code initializes a constant variable named "myVar" with an undefined value,
-* and then assigns a string value "JavaScript is amazing" to it. Finally, it logs
-* the value of myVar to the console using the console.log() function.
-*/
-
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
