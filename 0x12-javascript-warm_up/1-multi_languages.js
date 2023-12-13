87bce73188f04871d538581bccaff272fa2760f6
Mirai500 <asifatumahbub001@gmail.com>
2023-12-13 04:17:05 +0100

    0x12-javascript-warm_up-project-redo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-// Displaying multi-line text to the console.
-//
-
-const msg = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(msg);
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-// Print the values of argv except undefined.
-
-
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
